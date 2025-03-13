20c74f8dfceb39e23bf291b5942166f5e90b1a1a
Avngrstark62 <thakurabhijeetsingh79@gmail.com>
2025-03-13 12:37:46 +0000

    added docker

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API = axios.create({
     // baseURL: "http://localhost:8000",
     //   baseURL: "http://43.205.180.167:8000",
-       baseURL: "http://43.205.180.167/api/",
+       baseURL: "http://13.232.20.120/api/",
     withCredentials: true,
 });
 
