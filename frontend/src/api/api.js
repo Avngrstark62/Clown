b686f43740051821fcdccdf6ce3d198321249c5c
Abhijeet Singh Thakur <thakurabhijeetsingh79@gmail.com>
2025-03-15 21:51:23 +0530

    development testing

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const API = axios.create({
     baseURL: "http://localhost:8000",
-    //   baseURL: "http://43.205.180.167:8000",
-    //    baseURL: "http://13.232.20.120/api/",
     withCredentials: true,
 });
 
